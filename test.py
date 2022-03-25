6b41937348a4a72f817921e2165a73de029e5f97
Jinny268 <101381590+Jinny268@users.noreply.github.com>
2022-03-25 14:03:31 +0900

    Update test.py
    소개 부분을 수정했음.

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-print('김수정 202100776 컴퓨터공학부')
+# Touch by Jinny(2022-03-25)
+print('학과: 컴퓨터공학부')
+print('학번: 202100776')
+print('이름: 김수정')
+
 # touched by 417crystal
 # pullrequest practice-1
 # pullrequest practice-2
